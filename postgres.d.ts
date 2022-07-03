dab263b856b44f35c37413c1f717ff914929a80f
joshxyzhimself <joshxyzhimself@gmail.com>
2022-07-03 11:39:09 +0800

    fix return types

--- postgres.d.ts
+++ postgres.d.ts
@@ -28,22 +28,22 @@ export const create_table: create_table;
 export type validate_item = (table: table, item: item, null_id: boolean) => void;
 export const validate_item: validate_item;
 
-export type create_items = (sql: sql, table: table, items: item[]) => Promise<item[]>;
+export type create_items = (sql: sql, table: table, items: item[]) => Promise<any[]>;
 export const create_items: create_items;
 
-export type read_items = (sql: sql, table: table, limit: number, offset: number) => Promise<item[]>;
+export type read_items = (sql: sql, table: table, limit: number, offset: number) => Promise<any[]>;
 export const read_items: read_items;
 
-export type read_items_where = (sql: sql, table: table, name: string, operator: string, value: boolean|string|number, limit: number, offset: number) => Promise<item[]>;
+export type read_items_where = (sql: sql, table: table, name: string, operator: string, value: boolean|string|number, limit: number, offset: number) => Promise<any[]>;
 export const read_items_where: read_items_where;
 
-export type read_item = (sql: sql, table: table, id: number) => Promise<item>;
+export type read_item = (sql: sql, table: table, id: number) => Promise<any>;
 export const read_item: read_item;
 
-export type read_item_where = (sql: sql, table: table, name: string, operator: string, value: boolean|string|number) => Promise<item>;
+export type read_item_where = (sql: sql, table: table, name: string, operator: string, value: boolean|string|number) => Promise<any>;
 export const read_item_where: read_item_where;
 
-export type update_item = (sql: sql, table: table, item: item) => Promise<item>;
+export type update_item = (sql: sql, table: table, item: item) => Promise<any>;
 export const update_item: update_item;
 
 export type delete_item = (sql: sql, table: table, id: number) => Promise<void>;
