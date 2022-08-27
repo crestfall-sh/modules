a9b4ae4bb34379a64e1f764eb5a6ec905cf61c93
joshxyzhimself <joshxyzhimself@gmail.com>
2022-08-28 06:56:33 +0800

    useLocalStorage: bug fixes

--- useLocalStorage.mjs
+++ useLocalStorage.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 
+// Changelog
+// - 1.21.3: reflect current items properly
+// - 1.21.3: reflect updated items properly
+
 import React from 'react';
 import { assert } from './assert.mjs';
 
@@ -13,10 +17,14 @@ import { assert } from './assert.mjs';
  */
 export const useLocalStorage = (key, default_value) => {
   assert(typeof key === 'string');
+
+  const current_item = localStorage.getItem(key);
+
   /**
    * @type {[T, React.Dispatch<T>]}
    */
-  const [value, set_value] = React.useState(JSON.parse(localStorage.getItem(key)) || default_value);
+  const [value, set_value] = React.useState(typeof current_item === 'string' ? JSON.parse(current_item) : default_value);
+
   React.useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
     /**
@@ -24,7 +32,12 @@ export const useLocalStorage = (key, default_value) => {
      */
     const storage_event_listener = (e) => {
       if (e.key === key) {
-        set_value(JSON.parse(localStorage.getItem(key)) || default_value);
+        const updated_item = localStorage.getItem(key);
+        if (typeof updated_item === 'string') {
+          set_value(JSON.parse(updated_item));
+        } else {
+          set_value(null);
+        }
       }
     };
     window.addEventListener('storage', storage_event_listener);
@@ -32,5 +45,6 @@ export const useLocalStorage = (key, default_value) => {
       window.removeEventListener('storage', storage_event_listener);
     };
   }, [key, default_value, value]);
+
   return [value, set_value];
 };
