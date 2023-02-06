eff89ddb141e09fdaed78bbb1b5b93dea4f5a848
joshxyzhimself <joshxyzhimself@gmail.com>
2023-02-06 15:41:48 +0800

    hs256: improve claims types with comments

--- hs256.d.ts
+++ hs256.d.ts
@@ -1,22 +1,97 @@
-// https://www.rfc-editor.org/rfc/rfc7519#section-4
 
 export interface header {
   alg: string;
   typ: string;
 }
 
+/**
+ * - https://www.rfc-editor.org/rfc/rfc7519#section-4
+ * - https://www.iana.org/assignments/jwt/jwt.xhtml
+ */
 export interface payload {
-  iat?: number; // issued at time
-  nbf?: number; // not before time
-  exp?: number; // expiration time
-  iss?: string; // issuer
+
+
+  // ======================
+  // Registered Claim Names
+  // https://datatracker.ietf.org/doc/rfc7519/
+  // ======================  
+  /**
+   * issued at time
+   */
+  iat?: number;
+  /**
+   * not before time
+   */
+  nbf?: number;
+  /**
+   * expiration time
+   */
+  exp?: number;
+  /**
+   * issuer
+   */
+  iss?: string;
+  /**
+   * audience
+   */
   aud?: string; // audience
-  sub?: string; // subject
-  jti?: string; // jwt id
-  role?: string;
-  email?: string;
-  scopes?: string[];
+  /**
+   * subject (user uuid)
+   */
+  sub?: string;
+  /**
+   * jwt id
+   */
+  jti?: string;
+  /**
+   * refresh token
+   */
   refresh_token?: string;
+
+
+  // ======================
+  // Public Claim Names
+  // https://www.iana.org/assignments/jwt/jwt.xhtml#claims
+  // ======================
+  /**
+   * role (postgrest spec compliant)
+   * - one role only
+   */
+  role?: string;
+  /**
+   * scope (oauth2 protocol spec compliant)
+   * - lower-kebab-case or lower_snake_case, space-delimited
+   */
+  scope?: string;
+
+
+  // ======================
+  // Private Claim Names
+  // ======================
+  /**
+   * username
+   */
+  username?: string;
+  /**
+   * email address
+   */
+  email?: string;
+  /**
+   * roles
+   * - lower-kebab-case, comma-delimited
+   */
+  roles?: string[];
+  /**
+   * scopes
+   * - lower-kebab-case, comma-delimited
+   * - permission examples: read, write
+   * - permission examples: create, read, update, delete
+   * - scope example: write:authorization
+   * - scope example: read:projects
+   */
+  scopes?: string[];
+
+
 }
 
 export interface token_data {
