322148f06249b9e6d76588c535923cdd83d6bfb7
joshxyzhimself <joshxyzhimself@gmail.com>
2023-02-10 17:23:08 +0800

    hs256: full browser compatibility with tests

--- hs256.mjs
+++ hs256.mjs
@@ -25,16 +25,16 @@ export const hmac_sha256 = (key, data) => hmac.hmac(sha256.SHA256, key, data);
 /**
  * @param {header} header
  * @param {payload} payload
- * @param {string} secret_b64
+ * @param {string} secret
  * @returns {string}
  */
-export const create_token = (header, payload, secret_b64) => {
+export const create_token = (header, payload, secret) => {
   assert(header instanceof Object);
   assert(header.alg === 'HS256');
   assert(header.typ === 'JWT');
   assert(payload instanceof Object);
-  assert(typeof secret_b64 === 'string');
-  const secret_buffer = base64.decode(secret_b64);
+  assert(typeof secret === 'string');
+  const secret_buffer = base64.decode(secret);
   const header_b64ue = base64.encodeURLSafe(utf8.encode(JSON.stringify(header))).replace(/=/g, '');
   const payload_b64ue = base64.encodeURLSafe(utf8.encode(JSON.stringify(payload))).replace(/=/g, '');
   const signature_data = utf8.encode(`${header_b64ue}.${payload_b64ue}`);
@@ -73,14 +73,29 @@ export const read_token = (token) => {
 };
 
 /**
+ * @param  {Uint8Array[]} parts
+ */
+export const merge = (...parts) => {
+  assert(parts instanceof Array);
+  parts.forEach((part) => {
+    assert(part instanceof Uint8Array);
+  });
+  const merged_length = parts.reduce((previous, current) => previous + current.byteLength, 0);
+  const merged = new Uint8Array(merged_length);
+  let offset = 0;
+  parts.forEach((part) => {
+    merged.set(part, offset);
+    offset += part.byteLength;
+  });
+  return merged;
+};
+
+/**
  * @param {string} token
- * @param {string} secret
  * @returns {token_data}
  */
-export const verify_token = (token, secret) => {
+export const verify_nbf_exp = (token) => {
   assert(typeof token === 'string');
-  assert(typeof secret === 'string');
-  const secret_buffer = Buffer.from(secret, 'base64');
   const [header_b64ue, payload_b64ue, signature_b64ue] = token.split('.');
   assert(typeof header_b64ue === 'string');
   assert(typeof payload_b64ue === 'string');
@@ -111,7 +126,24 @@ export const verify_token = (token, secret) => {
     const now = luxon.DateTime.now();
     assert(now < exp, 'ERR_INVALID_TOKEN_EXPIRATION_TIME');
   }
-  const verification_signature_buffer = hmac_sha256(secret_buffer, Buffer.concat([Buffer.from(header_b64ue), Buffer.from('.'), Buffer.from(payload_b64ue)]));
+  return { header, payload };
+};
+
+/**
+ * @param {string} token
+ * @param {string} secret
+ * @returns {token_data}
+ */
+export const verify_sig = (token, secret) => {
+  assert(typeof token === 'string');
+  assert(typeof secret === 'string');
+  const secret_buffer = base64.decode(secret);
+  const [header_b64ue, payload_b64ue, signature_b64ue] = token.split('.');
+  assert(typeof header_b64ue === 'string');
+  assert(typeof payload_b64ue === 'string');
+  assert(typeof signature_b64ue === 'string');
+  const { header, payload } = verify_nbf_exp(token);
+  const verification_signature_buffer = hmac_sha256(secret_buffer, merge(utf8.encode(header_b64ue), utf8.encode('.'), utf8.encode(payload_b64ue)));
   const signature_buffer = base64.decodeURLSafe(signature_b64ue);
   assert(verification_signature_buffer.length === signature_buffer.length);
   for (let i = 0, l = verification_signature_buffer.length; i < l; i += 1) {
--- hs256.test.mjs
+++ hs256.test.mjs
@@ -6,23 +6,6 @@ import * as luxon from 'luxon';
 import * as hs256 from './hs256.mjs';
 
 {
-  const secret = crypto.randomBytes(32).toString('base64');
-  console.log({ secret });
-  const header = { alg: 'HS256', typ: 'JWT' };
-  const payload = {
-    iat: luxon.DateTime.now().toSeconds(),
-    nbf: luxon.DateTime.now().toSeconds(),
-    exp: luxon.DateTime.now().plus({ hours: 6 }).toSeconds(),
-    role: 'anon',
-  };
-  const token = hs256.create_token(header, payload, secret);
-  console.log({ token });
-  const verified_token = hs256.verify_token(token, secret);
-  assert(verified_token instanceof Object);
-  console.log({ verified_token });
-  console.log('hs256.verify_token OK');
-}
-{
   try {
     const secret = crypto.randomBytes(32).toString('base64');
     const header = { alg: 'HS256', typ: 'JWT' };
@@ -33,13 +16,13 @@ import * as hs256 from './hs256.mjs';
       role: 'anon',
     };
     const token = hs256.create_token(header, payload, secret);
-    hs256.verify_token(token, secret);
+    hs256.verify_nbf_exp(token);
     throw new Error('Unexpected token verification.');
   } catch (e) {
     if (e.message !== 'ERR_INVALID_TOKEN_NOT_BEFORE_TIME') {
       throw e;
     }
-    console.log('ERR_INVALID_TOKEN_NOT_BEFORE_TIME OK');
+    console.log('Test for hs256.verify_nbf_exp ERR_INVALID_TOKEN_NOT_BEFORE_TIME: OK');
   }
 }
 {
@@ -53,12 +36,26 @@ import * as hs256 from './hs256.mjs';
       role: 'anon',
     };
     const token = hs256.create_token(header, payload, secret);
-    hs256.verify_token(token, secret);
+    hs256.verify_nbf_exp(token);
     throw new Error('Unexpected token verification.');
   } catch (e) {
     if (e.message !== 'ERR_INVALID_TOKEN_EXPIRATION_TIME') {
       throw e;
     }
-    console.log('ERR_INVALID_TOKEN_EXPIRATION_TIME OK');
+    console.log('Test for hs256.verify_nbf_exp ERR_INVALID_TOKEN_EXPIRATION_TIME: OK');
   }
+}
+{
+  const secret = crypto.randomBytes(32).toString('base64');
+  const header = { alg: 'HS256', typ: 'JWT' };
+  const payload = {
+    iat: luxon.DateTime.now().toSeconds(),
+    nbf: luxon.DateTime.now().toSeconds(),
+    exp: luxon.DateTime.now().plus({ hours: 6 }).toSeconds(),
+    role: 'anon',
+  };
+  const token = hs256.create_token(header, payload, secret);
+  const token_data = hs256.verify_sig(token, secret);
+  assert(token_data instanceof Object);
+  console.log('Test for hs256.verify_sig: OK');
 }
